8987d3b061272d9b5655e0ba0c020cf6d4e884df
DialecticalLaw <d.shmuratkin@mail.ru>
2023-12-23 20:12:30 +0300

    feat: solve stack and queue tasks

--- src/queue.js
+++ src/queue.js
@@ -14,20 +14,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
+  data = [];
 
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.data[0];
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    const obj = {
+      value: value,
+      next: null
+    }
+
+    if (this.data.length) {
+      this.data.at(-1).next = obj;
+    }
+    this.data.push(obj);
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const result = this.data[0];
+    this.data.shift();
+    return result.value;
   }
 }
 
